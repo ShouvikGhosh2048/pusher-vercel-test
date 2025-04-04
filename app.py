b4254b28b7d9a88e74464a441fb18a50789f8775
Shouvik Ghosh <shouvikg.cal@gmail.com>
2025-04-04 14:34:19 +0530

    Added pusher

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from dotenv import load_dotenv
+import pusher
+import os
+
+load_dotenv()
+app_id=os.getenv('app_id')
+key=os.getenv('key')
+secret=os.getenv('secret')
+cluster=os.getenv('cluster')
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return f"""
+        <!DOCTYPE html>
+        <head>
+        <title>Pusher Test</title>
+        <script src="https://js.pusher.com/8.4.0/pusher.min.js"></script>
+        <script>
+            var pusher = new Pusher('{key}', {{
+            cluster: '{cluster}'
+            }});
+
+            var channel = pusher.subscribe('my-channel');
+            channel.bind('my-event', function(data) {{
+            alert(JSON.stringify(data));
+            }});
+        </script>
+        </head>
+        <body>
+        <h1>Pusher Test</h1>
+        <p>
+            Try publishing an event to channel <code>my-channel</code>
+            with event name <code>my-event</code>.
+        </p>
+        </body>
+    """
+
+@app.route("/send")
+def send():
+    pusher_client = pusher.Pusher(app_id, key, secret, cluster=cluster, ssl=True)
+    pusher_client.trigger('my-channel', 'my-event', {'message': 'hello world'})
+    return "<p>Message sent</p>"
